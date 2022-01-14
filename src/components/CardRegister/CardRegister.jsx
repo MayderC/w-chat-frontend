da47642cf4eb495fc18b0d648d9f5481c917dd21
MayderC <maydercortes1@gmail.com>
2022-01-13 21:32:52 -0600

    add component cardRegister

--- src/components/CardRegister/CardRegister.jsx
+++ src/components/CardRegister/CardRegister.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useState } from "react";
+import { InputForm } from "../InputForms/InputForm";
+import { CButton } from "../CButton/CButton";
+import "./index.scss";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  rePassword: "",
+};
 
 export const CardRegister = () => {
+  const [inputValues, setInputValues] = useState(initialState);
+
+  const handleSetInputValue = (propertie, value) => {
+    if (Object.keys(initialState).includes(propertie)) {
+      const newState = { ...initialState };
+      newState[propertie] = value;
+      setInputValues(newState);
+    }
+  };
+
   return (
     <>
-     <h1>Register</h1> 
+      <section>
+        <form className="card-register">
+          <h2>Registarse</h2>
+          <InputForm
+            props={{
+              type: "text",
+              placeholder: "Username",
+              propInState: "username",
+              handleSetInputValue,
+            }}
+          ></InputForm>
+
+          <InputForm
+            props={{
+              type: "text",
+              placeholder: "@email",
+              propInState: "email",
+              handleSetInputValue,
+            }}
+          ></InputForm>
+
+          <InputForm
+            props={{
+              type: "text",
+              placeholder: "Contraseña",
+              propInState: "password",
+              handleSetInputValue,
+            }}
+          ></InputForm>
+
+          <CButton text="Registarme"></CButton>
+        </form>
+      </section>
     </>
-  )
-}
+  );
+};
