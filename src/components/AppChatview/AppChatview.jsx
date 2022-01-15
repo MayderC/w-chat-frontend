44c7d6e5c5966e0c603c484d2be442a418a62f1f
MayderC <maydercortes1@gmail.com>
2022-01-15 11:50:49 -0600

    add inputMessage component in chatView

--- src/components/AppChatview/AppChatview.jsx
+++ src/components/AppChatview/AppChatview.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./index.scss";
 import { Route, Routes } from "react-router-dom";
-import { InputForm } from "../InputForms/InputForm";
+import { InputMessage } from "../InputMessagge/InputMessage";
 
 export const AppChatview = () => {
   const [Message, setMessage] = useState({ msg: "" });
@@ -9,9 +9,14 @@ export const AppChatview = () => {
   return (
     <>
       <main className="main_chat">
-        <Routes>
-          <Route path="/:id" element={<h1> chat adsffff</h1>}></Route>
-        </Routes>
+        <div className="message_view">
+          <Routes>
+            <Route path="/:id" element={<h1> chat adsffff</h1>}></Route>
+          </Routes>
+        </div>
+        <div className="message_input">
+          <InputMessage></InputMessage>
+        </div>
       </main>
     </>
   );
