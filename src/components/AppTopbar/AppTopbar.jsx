8d7236409c9b74cee00457e05f96f8d974ba63bc
Mayder C <maydercortes1@gmail.com>
2022-09-01 02:39:19 -0600

    add btn to get out, of global chat

--- src/components/AppTopbar/AppTopbar.jsx
+++ src/components/AppTopbar/AppTopbar.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { Context } from "../../context/Context";
+import  profileDefault from '../../assets/img/user.png'
 import "./index.scss";
 
 export const AppTopbar = () => {
@@ -8,6 +9,9 @@ export const AppTopbar = () => {
   return (
     <>
       <nav className="topbar_chat">
+        <div className="profile-container">
+          <img src={profileDefault} alt=""/>
+        </div>
         <p>{context.PROFILE.username}</p>
       </nav>
     </>
--- src/components/TopbarPrivateChat/TopbarPrivateChat.jsx
+++ src/components/TopbarPrivateChat/TopbarPrivateChat.jsx
@@ -1,13 +1,21 @@
 import "./index.scss";
 import { useContext } from "react";
 import { socketContext } from "../../context/socket/SocketContext";
+import { useNavigate } from "react-router-dom";
 
 export const TopbarPrivateChat = () => {
+  let navigate = useNavigate();
   const { socket } = useContext(socketContext);
+
+  const handleGetOut = async()  => {
+    socket.disconnect()
+    navigate('/app')
+  }
+
   return (
     <>
       <div className="chat__topbar">
-        <span onClick={() => socket.disconnect()}>click</span>
+        <div onClick={ handleGetOut }>Salir</div>
       </div>
     </>
   );
